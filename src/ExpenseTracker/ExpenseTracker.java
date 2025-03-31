720952c1bb13c590941deca9cfbf4171a71a6db3
JD <barrerasjuan01@gmail.com>
2025-03-31 09:26:47 -0700

    Added Error handling and input validation

--- src/ExpenseTracker/ExpenseTracker.java
+++ src/ExpenseTracker/ExpenseTracker.java
@@ -1,22 +1,25 @@
 package ExpenseTracker;
 
 
+import java.util.InputMismatchException;
 import java.util.Scanner;
 
 public class ExpenseTracker {
+
     public static void main(String[] args){
         while (true){
-            System.out.println("Expense Tracker\n1. Manage Expenses\n2. View all  \n3. Summarize \n4. Monthly View\n5. Quit");
-            Scanner input = new Scanner(System.in);
-            int choice = input.nextInt();
+            System.out.println("\n\nExpense Tracker\n1. Manage Expenses\n2. View all  \n3. Summarize \n4. Monthly View\n5. Quit");
+            int choice = getValidatedInt();
             switch(choice){
                 case 1:
                     while(true) {
-                        System.out.println("Expense Manager");
+                        System.out.println("\n\nExpense Manager");
                         System.out.println("1. Add Expense");
                         System.out.println("2. Update Expense");
                         System.out.println("3. Delete Expense");
-                        int option = input.nextInt();
+
+                        int option = getValidatedInt();
+
                         switch (option) {
                             case 1:
                                 //begin add
@@ -26,9 +29,11 @@ public class ExpenseTracker {
                                 break;
                             case 3:
                                 //choose to delete
+                                System.out.println("Deleted");
                                 break;
                             default:
-                                System.out.println("Invalid option");
+                                System.out.println("Invalid Option\nPlease Try Again");
+                                continue;
                         }
                         break;
                     }
@@ -45,6 +50,19 @@ public class ExpenseTracker {
                 case 5:
                     System.exit(0);
                 default:
+                    System.out.println("Invalid Option\nPlease Try Again");
+            }
+        }
+    }
+
+    public static int getValidatedInt(){
+        Scanner input = new Scanner(System.in);
+        while(true){
+            try{
+                return input.nextInt();
+            }catch (InputMismatchException notInteger){
+                System.out.println("Please enter an integer:");
+                input.nextLine();
             }
         }
     }
