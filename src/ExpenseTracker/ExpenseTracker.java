5cfc7dd3935e36efbef8e44a9af572ac707faa2f
JD <barrerasjuan01@gmail.com>
2025-04-01 11:26:14 -0700

    Can now update and delete expense objects.

--- src/ExpenseTracker/ExpenseTracker.java
+++ src/ExpenseTracker/ExpenseTracker.java
@@ -1,13 +1,10 @@
 package ExpenseTracker;
-
-
 import java.util.ArrayList;
 import java.util.InputMismatchException;
 import java.util.Scanner;
 
 public class ExpenseTracker {
 
-    static int expenses = 0;
     static ArrayList<Expense> expenseList = new ArrayList<Expense>();
 
     public static void main(String[] args){
@@ -21,9 +18,7 @@ public class ExpenseTracker {
                         System.out.println("1. Add Expense");
                         System.out.println("2. Update Expense");
                         System.out.println("3. Delete Expense");
-
                         int option = getValidatedInt();
-
                         switch (option) {
                             case 1: // ADD EXPENSE
                                 Scanner input = new Scanner(System.in);
@@ -33,20 +28,54 @@ public class ExpenseTracker {
                                 String desc = input.nextLine();
                                 System.out.println("Enter the amount rounded in dollars");
                                 int amount = getValidatedInt();
-                                Expense expense = new Expense(expenses+1, date, desc, amount);
+                                Expense expense = new Expense(expenseList.size()+1, date, desc, amount);
                                 expenseList.add(expense);
                                 break;
-                            case 2:
+                            case 2: // UPDATE EXPENSE
+                                Scanner input2 = new Scanner(System.in);
                                 System.out.println("Which Expense would you like to update?:");
                                 displayExpenses();
                                 System.out.println("Enter ID:");
-                                int id = getValidatedInt()-1;
-                                System.out.println("Enter Description:");
-
+                                int updateID = getValidatedInt();
+                                if (updateID <= expenseList.size() && updateID > 0) {
+                                    System.out.println("What would you like to update?\n1.Date\n2.Description\n3.Amount");
+                                    int option2 = getValidatedInt();
+                                    switch (option2) {
+                                        case 1:
+                                            System.out.println("Enter the updated Date:");
+                                            expenseList.get(updateID-1).setDate(input2.nextLine());
+                                            System.out.println("Date updated.");
+                                            break;
+                                        case 2:
+                                            System.out.println("Enter the updated Description:");
+                                            expenseList.get(updateID-1).setDescription(input2.nextLine());
+                                            System.out.println("Description updated.");
+                                            break;
+                                        case 3:
+                                            System.out.println("Enter the updated Amount:");
+                                            expenseList.get(updateID-1).setAmount(getValidatedInt());
+                                            System.out.println("Amount updated.");
+                                            break;
+                                            default:
+                                                break;
+                                    }
+                                }
+                                else {
+                                    System.out.println("Could not find expense with ID #" + updateID);
+                                }
                                 break;
-                            case 3:
-                                //choose to delete
-                                System.out.println("Deleted");
+                            case 3: // DELETE EXPENSE
+                                System.out.println("Which Expense would you like to delete?:");
+                                displayExpenses();
+                                System.out.println("Enter ID:");
+                                int deleteID = getValidatedInt();
+                                if (deleteID <= expenseList.size() && deleteID > 0) {
+                                    expenseList.remove(deleteID-1);
+                                    System.out.println("Expense deleted.");
+                                }
+                                else {
+                                    System.out.println("Could not find expense with ID #" + deleteID);
+                                }
                                 break;
                             default:
                                 System.out.println("Invalid Option\nPlease Try Again");
@@ -56,10 +85,10 @@ public class ExpenseTracker {
                     }
                     break;
                 case 2:
-                    //display all
+                    displayExpenses();
                     break;
                 case 3:
-                    //summarize by category
+                    System.out.println("Summarizing Expenses:");
                     break;
                 case 4:
                     //monthly view
